2f0ed35ec7daa8c3f1da33cdb2c7d986dec133e7
황수빈 <@DESKTOP-GQ05KV9>
2023-05-30 19:59:06 +0900

    sts3 HelloController 수정

--- git_boot/src/main/java/com/example/demo/HelloController.java
+++ git_boot/src/main/java/com/example/demo/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 	@RequestMapping("/helloboot")
 	public ModelAndView helloBoot() {
+		System.out.println("sts3에서 추가한 문장입니다.");
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("model", "스프링부트를 시작합니다(github).");
 		mv.setViewName("hello");
